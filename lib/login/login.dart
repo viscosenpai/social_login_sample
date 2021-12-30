eaaa7231019cec1a7b293e0253cd64f2db0a4fcc
viscosenpai <kama.hiroki@gmail.com>
2021-12-30 15:21:53 +0900

    [add] Apple Sign In 対応@

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,26 +1,29 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:crypto/crypto.dart';
 import '../app.dart';
 
-// import 'package:google_sign_in/google_sign_in.dart';
+/// Generates a cryptographically secure random nonce, to be included in a
+/// credential request.
+String generateNonce([int length = 32]) {
+  const charset =
+      '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+  final random = Random.secure();
+  return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+      .join();
+}
 
-// Future<UserCredential> signInWithGoogle() async {
-//   // Trigger the authentication flow
-//   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-//   // Obtain the auth details from the request
-//   final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-
-//   // Create a new credential
-//   final credential = GoogleAuthProvider.credential(
-//     accessToken: googleAuth?.accessToken,
-//     idToken: googleAuth?.idToken,
-//   );
-
-//   // Once signed in, return the UserCredential
-//   return await FirebaseAuth.instance.signInWithCredential(credential);
-// }
+/// Returns the sha256 hash of [input] in hex notation.
+String sha256ofString(String input) {
+  final bytes = utf8.encode(input);
+  final digest = sha256.convert(bytes);
+  return digest.toString();
+}
 
 class Login extends StatelessWidget {
   Login({Key? key}) : super(key: key);
@@ -63,7 +66,40 @@ class Login extends StatelessWidget {
                   }
                 },
                 child: const Text('Google 認証'),
-              )
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final rawNonce = generateNonce();
+                  final nonce = sha256ofString(rawNonce);
+
+                  final appleCredential =
+                      await SignInWithApple.getAppleIDCredential(
+                    scopes: [
+                      AppleIDAuthorizationScopes.email,
+                      AppleIDAuthorizationScopes.fullName,
+                    ],
+                    nonce: nonce,
+                  );
+                  final oauthCredential = OAuthProvider("apple.com").credential(
+                    idToken: appleCredential.identityToken,
+                    rawNonce: rawNonce,
+                  );
+
+                  try {
+                    UserCredential result = await FirebaseAuth.instance
+                        .signInWithCredential(oauthCredential);
+                    User? user = result.user;
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => App(userId: user!.uid),
+                        ));
+                  } catch (e) {
+                    print(e);
+                  }
+                },
+                child: const Text('Apple 認証'),
+              ),
             ],
           ),
         ),
